717d7884c1b395a494ada3c57ea1036c1e7656a6
Patricianny Barreto <45316592+Patriciany@users.noreply.github.com>
2020-03-27 23:15:38 -0300

    Create Conjunto.py

---
+++ Conjunto.py
@@ -0,0 +1,14 @@
+conjuntoA = {1, 2, 3, 4, 5}
+conjuntoB = {4, 5, 6, 7, 8}
+
+#União
+uniao = conjuntoA | conjuntoB
+print(uniao)
+
+#Interseção
+intersecao = conjuntoA & conjuntoB
+print(intersecao)
+
+#Diferença
+diferecia = conjuntoA - conjuntoB
+print(diferecia)
